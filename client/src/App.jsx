363270fb22c98d44195c9721a41456df5b17b3a8
Steven Thao <sthao21@csuchico.edu>
2024-05-29 22:46:10 -0700

    Updated Homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,14 +13,15 @@ const App = () => {
         </Link>
 
         <Link to='/create-post' className='font-inter font-medium bg-[#6469ff] text-white py-2 px-4 rounded'>
-          Create Post
+          Create
         </Link>
       </header>
-
+      <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/create-post' element={<CreatePost />} />
-      </Routes>
+      </Routes>  
+      </main> 
     </BrowserRouter>
   );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-const Home = () => {
+import { Loader, Card, FormField } from '../components'
+
+const RenderCards = ({data, title}) => {
+    if (data?.length > 0) {
+        return data.map((post) => <Card key={post._id} {...post} />)
+    }
+
     return (
-        <div>Home</div>
+        <h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>{title}</h2>
+    )
+}
+const Home = () =>{
+    const [loading, setloading] = useState(false);
+    const [allPosts, setAllPosts] = useState(null);
+
+    const [searchText, setSearchText] = useState('');
+    return (
+        <section className='max-w-7xl mx-auto'>
+            <div>
+                <h1 className='font-extrabold text-[#222328] text-[32px]'>
+                    The Community Showcase 
+                </h1>
+                <p className='mt-2 text-[#666e75] text-[16px] max-w[500px]'> Text goes here </p>
+            </div>
+            <div className='mt-10'>
+                <FormField />
+            </div>
+
+            <div className='mt-10'>
+                {loading ? (
+                    <div className='flex justify-center items-center'>
+                    <Loader />
+                    </div>
+                ) : (
+                    <>
+                        {searchText && (
+                            <h2 className='font-medium text-[#666e75]'>
+                                Show result from <span className='text-[222328]'>{search}</span>
+                            </h2>
+                        )}
+                        <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
+                            {searchText ? (
+                                <RenderCards data={[]} title='Search Result' />
+                                ) : (
+                                    <RenderCards data={[]} title="No post found" />
+                            )}
+                        </div>
+                    </>
+                )}
+            </div>
+        </section>
     )
 }
 
